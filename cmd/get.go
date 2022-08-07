d0d7aed9455fce99d12f5bc7f1d495efdc16caa3
Julien Vinet <julien@vinet.dev>
2022-08-07 19:39:20 +0200

    multithreading

--- cmd/get.go
+++ cmd/get.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/knackybear/goquery/pkg/utils"
 	"github.com/spf13/cobra"
@@ -15,22 +17,41 @@ var cmdGet = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	ValidArgsFunction:     cobra.NoFileCompletions,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		rc := make(chan *http.Response)
+		queue := []string{}
+		
 		file, err := os.Open(currentConfig.Csv)
 		if err != nil {
 			return err
 		}
 		defer file.Close()
 		vars := utils.CSVToMap(file)
+
 		for _, variable := range vars {
 			for _, url := range currentConfig.Url {
 				for k, v := range variable {
 					url = strings.ReplaceAll(url, fmt.Sprintf("{%s}", k), v)
 				}
-				println("GET ", url)
+				queue = append(queue, url)
 			}
+		}
+
+		var wg sync.WaitGroup
+		wg.Add(len(queue))
+
+		for idx, req := range queue {
+			go func()
 		}
 		return nil
 	},
+}
+
+func GetRequest(url string, rc chan *http.Response) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		rc <- resp
+	}
 }
 
 func init() {
--- pkg/utils/job.go
+++ pkg/utils/job.go
@@ -3,3 +3,57 @@ package utils
 type Job struct {
 	result interface{}
 }
+
+func (self *Job) execute() {
+	// do some network activity
+}
+  
+  // to be executed parallelly
+doParallel := func(ctx context.Context, inputs <-chan Job, output chan<- Job) {
+	for {
+		select {
+		case tag, ok := <-inputs:
+		if !ok {
+			return
+		}
+		job.result := job.execute()
+		output <- job
+		case <-ctx.Done():
+		return
+		}
+	}
+}
+  
+  // an array of jobs
+  jobs := make([]Job)
+  jobs.append(jobs,...)
+  
+  ctx := context.Background()
+  max := runtime.NumCPU()
+  queue := make(chan Job, max)
+  output := make(chan Job)
+  defer close(output)
+  
+  // spin up workers
+  for i := 0; i < max; i++ {
+	go doParallel(ctx, queue, output)
+  }
+  
+  // passing jobs to workers though a queue, idle workers will pick the job and execute
+  go func() {
+	for _, job := range jobs {
+	  queue <- job
+	}
+	close(queue)
+  }()
+  
+  // collecting results
+  results := make([]Job, len(jobs))
+  for i := 0; i < len(jobs); i++ {
+	select {
+	case executedJob := <-output:
+	  results[i] = executedJob
+	case <-ctx.Done():
+	  break
+	}
+  }
